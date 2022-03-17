d4d3fbe5075fdb117fe098707fc14ccce70ec72e
june1921 <zone1921@naver.com>
2022-03-17 16:34:18 +0900

    로그인 업데이트

--- base/ggcafe_login_control/ggcafe/src/main/java/com/cosla/ggcafe/controller/UserController.java
+++ base/ggcafe_login_control/ggcafe/src/main/java/com/cosla/ggcafe/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.cosla.ggcafe.controller;
 
+import com.cosla.ggcafe.model.OAuthToken;
+import com.cosla.ggcafe.repository.UserRepository;
 import com.cosla.ggcafe.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +16,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @CrossOrigin
 @Controller
 public class UserController {
-   
-    //@Autowired
-    
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/auth/kakao/callback") //redirect_uri : /auth/kakao/callback
     @ResponseBody
-    public String kakaoCallback(String code) {
+    public KakaoProfile kakaoCallback(String code) {
         UserService userService = new UserService();
-        userService.getAccessToken(code);
-        return "코드는 = " + code;
+        OAuthToken oAuthToken = new OAuthToken();
+        oAuthToken = userService.getAccessToken(code);
+        
+        return userService.getProfile(oAuthToken, userRepository);
     
+        //https://kauth.kakao.com/oauth/authorize?client_id=db7f399f1838c8f409b9611791d7f87a&redirect_uri=http://localhost:8080/auth/kakao/callback&response_type=code
     }
     
 }
