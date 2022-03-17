7fcf4e1b38ab254f80360e4e20f993b493936d74
hans <focus5n@naver.com>
2022-03-17 15:41:55 +0900

    API 호출 && 간단 UI 구현 => 앞으로 백엔드 만들어서 NewsAPI 대체

--- base/ggcafe_front/src/components/ExpertList.js
+++ base/ggcafe_front/src/components/ExpertList.js
@@ -1,7 +1,10 @@
-import React from "react";
+// Libraries
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
-import ExpertItem from "./components/ExpertItem";
+// Components
+import ExpertItem from "./ExpertItem";
 
 const ExpertListBlock = styled.div`
   box-sizing: border-box;
@@ -16,21 +19,41 @@ const ExpertListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "http://ggoreb.com",
-  urlToImage: "https://via.placeholder.com/160",
-};
-
 const ExpertList = () => {
+  const [experts, setExperts] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines" +
+            "?country=kr&apiKey=0c26f7a177e5434b9e719449d2dc0e97"
+        );
+        setExperts(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <ExpertListBlock>대기 중...</ExpertListBlock>;
+  }
+  if (!experts) {
+    return null;
+  }
+
   return (
     <ExpertListBlock>
-      <ExpertItem article={sampleArticle} />
-      <ExpertItem article={sampleArticle} />
-      <ExpertItem article={sampleArticle} />
+      {experts.map((experts) => {
+        return <ExpertItem key={experts.url} article={experts} />;
+      })}
     </ExpertListBlock>
   );
 };
 
-export default ExpertItem;
+export default ExpertList;
