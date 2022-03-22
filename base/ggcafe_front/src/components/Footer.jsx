19dd314db01ad3bf0b772aa9dc14b1a28cc00fdc
Hans <focus5n@naver.com>
2022-03-22 18:03:08 +0900

    header / footer

--- base/ggcafe_front/src/components/Footer.jsx
+++ base/ggcafe_front/src/components/Footer.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-function Footer(props) {
-  return <div></div>;
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      Team Cosla
+      {new Date().getFullYear()}
+    </Typography>
+  );
 }
 
-export default Footer;
+export default function StickyFooter() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <CssBaseline />
+      <Container
+        component="main"
+        sx={{ mt: 8, mb: 2 }}
+        maxWidth="sm"
+      ></Container>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
--- base/ggcafe_front/src/components/Header.jsx
+++ base/ggcafe_front/src/components/Header.jsx
@@ -1,42 +1,159 @@
-// Libraries
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-// Components
-
+// Header CSS
 const StyledHeader = styled.div`
-  .body {
+  body {
     margin: 0%;
   }
 
   a {
     text-decoration: none;
     color: #0c140e;
+    font-family: "Noto Sans CJK KR";
   }
 
   .Header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: #a97a5e;
+    background-color: #e6ddd6;
     padding: 8px 12px;
   }
 
-  .Header .Header-logo {
+  /* --- Bar logo --- */
+  .Header-logo {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     font-size: 24px;
-    color: aqua;
+    color: #6f4b32;
+    margin-left: 8px;
+  }
+
+  .Header-logo a {
+    padding: 8px;
+  }
+
+  /* --- Bar menu --- */
+  .Header-menu {
+    display: flex;
+    list-style: none;
+    padding-left: 0%;
+  }
+
+  .Header-menu li {
+    padding: 8px 12px;
+  }
+
+  .Header-menu li:hover {
+    background-color: beige;
+    border-radius: 4px;
+  }
+
+  /* --- Bar icons --- */
+  .Header-icons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    list-style: none;
+    padding-left: 0%;
+  }
+
+  .Header-icons a {
+    padding: 8px 12px;
+    color: #0c140e;
+  }
+
+  /* Bar toggle-button */
+  .toggle-button {
+    display: none;
+  }
+
+  /* --- flexible --- */
+  @media screen and (max-width: 768px) {
+    .Header {
+      flex-direction: column;
+    }
+
+    /* --- Mobile Logo --- */
+    .Header-logo {
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+      padding: 8px 12px;
+      margin: 0%;
+    }
+
+    .Header-logo a {
+      display: none;
+    }
+
+    /* --- Mobile Menu --- */
+
+    .Header-menu {
+      display: none;
+      flex-direction: column;
+      width: 100%;
+      padding: 8px 12px;
+      padding-left: 0%;
+      margin: 0%;
+    }
+
+    .Header-menu a {
+      width: 100%;
+      padding: 0%;
+    }
+
+    .Header-menu.active,
+    .Header-icons.active {
+      display: flex;
+    }
+
+    /* --- Mobile Icons --- */
+
+    .Header-icons {
+      display: none;
+      justify-content: center;
+      width: 100%;
+    }
+
+    .Header-icons.active {
+      display: flex;
+    }
+
+    .toggle-button {
+      display: block;
+      position: absolute;
+      font-size: 24px;
+      right: 24px;
+      color: #6f4b32;
+    }
   }
 `;
+
+// toggle-button Event
+function toggleMenu() {
+  const toggleMenu = document.querySelector(".toggle-button");
+  const menu = document.querySelector(".Header-menu");
+  const icons = document.querySelector(".Header-icons");
+
+  return toggleMenu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    icons.classList.toggle("active");
+  });
+}
+
 function Header(props) {
   return (
     <StyledHeader>
       <div className="Header">
         {/* Logo */}
         <div className="Header-logo">
+          <i className="fa-solid fa-mug-saucer"></i>
           <NavLink to="/">
-            <i className="fa-solid fa-mug-saucer"></i>
-            공감 한 잔
+            <p>공감 한 잔</p>
           </NavLink>
         </div>
 
@@ -49,25 +166,34 @@ function Header(props) {
             <NavLink to="/matching">전문가 찾기</NavLink>
           </li>
           <li>
-            <NavLink to="/counselling">상담 시작하기</NavLink>
+            <NavLink to="/column">전문가 칼럼</NavLink>
           </li>
           <li>
-            <NavLink to="/column">전문가 칼럼</NavLink>
+            <NavLink to="/counselling">상담 받기</NavLink>
           </li>
           <li>
             <NavLink to="/information">고객센터</NavLink>
           </li>
         </ul>
 
-        {/* Navigation Icons */}
-        <div className="Header-icons">
-          <NavLink to="/memberinfo">
-            <i className="fa-solid fa-address-card"></i>
-          </NavLink>
-          <NavLink to="/signout">
-            <i className="fa-solid fa-right-from-bracket"></i>
-          </NavLink>
-        </div>
+        {/* Navigation icons */}
+        <ul className="Header-icons">
+          <li>
+            <NavLink to="/memberinfo">
+              <i className="fa-solid fa-address-card"></i>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/signout">
+              <i className="fa-solid fa-right-from-bracket"></i>
+            </NavLink>
+          </li>
+        </ul>
+
+        {/* toggle-button */}
+        <NavLink to="#" className="toggle-button" onClick={toggleMenu}>
+          <i className="fa-solid fa-bars"></i>
+        </NavLink>
       </div>
     </StyledHeader>
   );
