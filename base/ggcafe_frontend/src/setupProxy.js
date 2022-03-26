733f209f3a80fe1c5ceb0afbaea9ef955e3ba9d5
Hans <focus5n@naver.com>
2022-03-27 01:17:54 +0900

    전문가 상세정보까지 완료

--- base/ggcafe_frontend/src/setupProxy.js
+++ base/ggcafe_frontend/src/setupProxy.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-export default function(app) {
+export default function (app) {
   app.use(
     createProxyMiddleware("/api", {
       target: "http://localhost:8081",
