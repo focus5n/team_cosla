6f824419ac5462b9ed5d1dfaeb13757e4a32fac8
Hans <focus5n@naver.com>
2022-03-26 23:27:00 +0900

    수정

--- base/ggcafe_frontend/src/setupProxy.js
+++ base/ggcafe_frontend/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-module.exports = function (app) {
+export default function(app) {
   app.use(
     createProxyMiddleware("/api", {
       target: "http://localhost:8081",
       changeOrigin: true,
     })
   );
-};
+}
