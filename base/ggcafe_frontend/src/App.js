3da22c5dc1324efd53a161c7f8c9dfd132e85c1e
Hans <focus5n@naver.com>
2022-04-16 14:21:50 +0900

    통합

--- base/ggcafe_frontend/src/App.js
+++ base/ggcafe_frontend/src/App.js
@@ -14,13 +14,7 @@ import Diary from "./components/diary/Diary";
 // 전문가 목록
 import ExpertListService from "./services/expertService/ExpertListService";
 // 전문가 상세정보
-<<<<<<< HEAD
-import MatchingDetailService from "./services/matchingService/MatchingDetailService";
-// 일정 선택
-import Calendar from "./components/matching/Calendar";
-=======
 import ExpertDetailService from "./services/expertService/ExpertDetailService";
->>>>>>> 8b420b3ea16ffc1ce372e4f37f8e883e386185ee
 // 전문가 칼럼
 import Column from "./components/column/Column";
 // 상담 받기
@@ -42,23 +36,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-<<<<<<< HEAD
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/diary" element={<Diary />} />
-          <Route path="/matchinglist" element={<MatchingListService />} />
-          <Route
-            path="/matchingdetail/:id"
-            element={<MatchingDetailService />}
-          />
-          <Route path="/schedule" element={<Calendar />} />
-          <Route path="/column" element={<Column />} />
-          <Route path="/counselling" element={<Counselling />} />
-          <Route path="/information" element={<Information />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/callback/kakao" element={<Login />}></Route>
-=======
         <Routes path="/">
           <Route index path="" element={<Home />} />
           <Route path="diary" element={<Diary />} />
@@ -70,7 +47,6 @@ function App() {
           <Route path="signin" element={<SignIn />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="callback/kakao" element={<Login />}></Route>
->>>>>>> 8b420b3ea16ffc1ce372e4f37f8e883e386185ee
         </Routes>
         <Footer />
       </BrowserRouter>
--- base/ggcafe_frontend/src/setupProxy.js
+++ base/ggcafe_frontend/src/setupProxy.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-/* eslint-disable import/no-anonymous-default-export */
-import { createProxyMiddleware } from "http-proxy-middleware";
-
-export default function (app) {
-=======
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
->>>>>>> 8b420b3ea16ffc1ce372e4f37f8e883e386185ee
-  app.use(
-    createProxyMiddleware("/api", {
-      target: "localhost:8081", //localhost:8080 등 설정
-      changeOrigin: true,
-    })
-  );
-}
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("/api", {
+//       target: "localhost:8081", //localhost:8080 등 설정
+//       changeOrigin: true,
+//     })
+//   );
+// };
