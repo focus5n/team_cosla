e81495994841e84ed54ba96aa5453e845564cd7a
Hans <focus5n@naver.com>
2022-04-21 01:46:33 +0900

    별도의 폴더에서 작업한 것 옮김

---
+++ ggcafe/frontend/src/App.js
@@ -0,0 +1,59 @@
+// Libraries
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Header & Footer
+import Header from "./components/home/Header";
+import Footer from "./components/home/Footer";
+
+// Section Components
+// 메인 화면
+import Home from "./components/home/Home";
+// 공감 일기장
+import Diary from "./components/diary/Diary";
+// 전문가 목록
+import ExpertListService from "./services/expertService/ExpertListService";
+// 전문가 상세정보
+import ExpertDetailService from "./services/expertService/ExpertDetailService";
+// 매칭
+import MatchService from "./services/matchService/MatchService";
+// 매칭 결과
+import Result from "./components/match/Result";
+// 로그인
+import SignIn from "./components/auth/SignIn";
+// 회원가입
+import SignUp from "./components/auth/SignUp";
+// 로그인 콜백
+import Login from "./components/auth/Login";
+
+// CSS
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes path="/">
+          {/* 메인화면 */}
+          <Route index path="" element={<Home />} />
+          {/* 일기장 */}
+          <Route path="diary" element={<Diary />} />
+          {/* 전문가 */}
+          <Route path="expert" element={<ExpertListService />} />
+          <Route path="detail/:id" element={<ExpertDetailService />} />
+          {/* 매치 */}
+          <Route path="match/:id" element={<MatchService />} />
+          <Route path="result/:id" element={<Result />} />
+          {/* 로그인 */}
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="callback/kakao" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ggcafe/frontend/src/services/matchService/MatchService.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Match from "../../components/match/Match";
+
+function MatchService(props) {
+  const [data, setData] = useState(null);
+  const { id } = useParams();
+  const [test, setTest] = useState("");
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(`http://localhost:8080/expert/${id}`);
+      setData(response.data);
+    };
+    fetch();
+  }, [id]);
+
+  if (!data) {
+    return null;
+  }
+
+  return <Match key={data.urlToImage} info={data} setTest={setTest}></Match>;
+}
+
+export default MatchService;
