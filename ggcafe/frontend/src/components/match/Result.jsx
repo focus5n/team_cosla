6f7928958c10cff0cff9091d8a87ebaa94f06573
hans <focus5n@naver.com>
2022-04-21 20:30:04 +0900

    Coding Result

--- ggcafe/frontend/src/components/match/Result.jsx
+++ ggcafe/frontend/src/components/match/Result.jsx
@@ -1,10 +1,70 @@
 import React from "react";
+import styled from "styled-components";
 
-function Result(props) {
+// CSS
+const StyledResult = styled.div`
+  .container {
+    display: flex;
+  }
+`;
+
+// Parts
+const Picture = () => {
+  return <div className="picture">{urlToImage}</div>;
+};
+
+const Detail = () => {
   return (
-    <div className="container">
-      <div className="card"></div>
+    <div className="detail">
+      <div className="purpose">{matchPurpose}</div>
+      <div className="type">{matchType}</div>
     </div>
+  );
+};
+
+const Name = () => {
+  return (
+    <div className="name">
+      <div className="userName">{userName}</div>
+      <div className="expertName">{expertName}</div>
+    </div>
+  );
+};
+
+const Date = () => {
+  return (
+    <div className="date">
+      <div className="matchDate">{matchDate}</div>
+    </div>
+  );
+};
+
+// Function
+function Result(props) {
+  const {
+    urlToImage,
+    matchPurpose,
+    matchType,
+    userName,
+    expertName,
+    matchDate,
+  } = props.data;
+
+  return (
+    <StyledResult>
+      <div className="container">
+        <div className="card">
+          <div className="image">
+            <Picture />
+          </div>
+          <div className="info">
+            <Name />
+            <Detail />
+            <Date />
+          </div>
+        </div>
+      </div>
+    </StyledResult>
   );
 }
 
